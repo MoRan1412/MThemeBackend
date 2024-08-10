c8c4c6c22cfd68b5d774659743abd50e83bef293
MoRan <ooklibaioo@gmail.com>
2024-08-11 01:08:46 +0800

    MThemeAPI

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const rand = require('csprng');
+const mysql = require('mysql');
+const app = express();
+const port = 10888  // Replit doesn’t matter which port is using
+
+app.use(bodyParser.json()); //Used to parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true })); //Parse URL-encoded bodies
+
+const tokenUsers = []
+
+const status = {
+    OK: 200,
+    CREATED: 201,
+    NOT_MODIFIED: 304,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+};
+
+const db = mysql.createConnection({
+    host: 'ec2-43-199-88-244.ap-east-1.compute.amazonaws.com',
+    port: '3306',
+    user: 'root',
+    password: 'W836rrv+',
+    database: 'mtheme',
+});
+
+db.connect(function (err) {
+
+    if (!err) {
+        console.log("Database is connected ... ");
+    } else {
+        console.log("Error connecting database ... ");
+    }
+});
