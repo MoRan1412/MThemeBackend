d51e32d1a1ebf6961cc5dda5a04590fcd98c9226
MoRan <ooklibaioo@gmail.com>
2024-08-11 20:45:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ const db = mysql.createConnection({
 
 db.connect()
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(port, () => {
     console.log(`Connected on port ${port}`)
 }); 
